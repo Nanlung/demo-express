8709cc1a005dba3e1c48818941258c8890eb09c3
Nanlung <nanlung.temlong@gmail.com>
2022-05-11 04:28:27 +0100

    route fix

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Running on Heroku!!!')
+  res.status(200).json({message: "Show me off"})
 })
 
 
